72b6bdfa298004dd15833f424aeeff3c218f7967
BeckyZhuWu <zhuwub@gmail.com>
2024-04-21 14:03:25 -0600

    agregar boton favoritos

--- Front/frontBRIW/src/Resultado.tsx
+++ Front/frontBRIW/src/Resultado.tsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "./context/UserContext";
 
 function ResultadoBusqueda({ titulo, snippet, logo, url }) {
-    return (
-      <div className=" border-secondary p-4 rounded-lg shadow-md flex items-center space-x-4 mr-12 z-0">
-        {logo && (
-            <div className="w-12 h-12 bg-neutral rounded-full flex justify-center">
-              <img  className="rounded-full" src={logo} alt="page logo"/> 
-            </div>
-        )}
-        <div className="flex-grow">
-          <a href={url} className="font-medium text-accent hover:underline" target="_blank" rel="noopener noreferrer">
-            {titulo}
-          </a>
-          <p className="text-gray-500">{snippet}</p>
-          <a href={url} className="text-sm text-gray-400 hover:text-gray-600 hover:underline" target="_blank" rel="noopener noreferrer">
-            {url}
-          </a>
+  const { favoritos, setFavoritos } = useContext(UserContext);
+
+  const shouldAddToFavorites = () => {
+    if (favoritos.some((favorito) => favorito.url === url)) {
+      setFavoritos(favoritos.filter((favorito) => favorito.url !== url));
+    } else {
+      setFavoritos([...favoritos, { titulo, url, logo }]);
+    }
+  };
+
+  return (
+    <div className=" border-secondary p-4 rounded-lg shadow-md flex items-center space-x-4 mr-12 z-0">
+      {logo && (
+        <div className="w-12 h-12 bg-neutral rounded-full flex justify-center">
+          <img className="rounded-full" src={logo} alt="page logo" />
         </div>
+      )}
+      <div className="flex-grow">
+        <a
+          href={url}
+          className="font-medium text-accent hover:underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {titulo}
+        </a>
+        <p className="text-gray-500">{snippet}</p>
+        <a
+          href={url}
+          className="text-sm text-gray-400 hover:text-gray-600 hover:underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {url}
+        </a>
       </div>
-    );
-  }
-  
-  export default ResultadoBusqueda;
-  
+      <button id="favButton" onClick={() => shouldAddToFavorites()}>
+        <span className="material-icons-outlined">
+          {favoritos.some((favorito) => favorito.url === url)
+            ? "star"
+            : "star_border"}
+        </span>
+      </button>
+    </div>
+  );
+}
+
+export default ResultadoBusqueda;
